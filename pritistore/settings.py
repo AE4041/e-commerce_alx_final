75c7ca272ac682a828fc9cd8235f5658e9e59abb
Emmanuel <asamoahe304@gmail.com>
2024-12-15 12:58:35 +0000

    ss

--- pritistore/settings.py
+++ pritistore/settings.py
@@ -135,19 +135,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-# STATIC_URL = '/static/'
-# #STATIC_ROOT = BASE_DIR / 'static'
-# STATIC_ROOT = os.path.join(BASE_DIR, "/static")
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'pritistore/static',  # Updated project name
-# ]
-# # STATICFILES_DIRS = [os.path.join(BASE_DIR, "pritistore/static")]
-# STATIC_URL = '/static/'
-# #STATIC_ROOT = BASE_DIR / 'staticfiles'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [BASE_DIR / 'static']  # Add this line if your static files are in 'templates'
-
-
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'   # For collectstatic output
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'pritistore/static')]  # Static files in 'pritistore/static'
