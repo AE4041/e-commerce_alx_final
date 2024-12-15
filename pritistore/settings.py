71386a7d70a4c5e43a55ac92918bbb6a4211af01
Emmanuel <asamoahe304@gmail.com>
2024-12-15 10:48:37 +0000

    refined

--- pritistore/settings.py
+++ pritistore/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 #STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [BASE_DIR / 'templates']  # Add this line if your static files are in 'templates'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # Add this line if your static files are in 'templates'
 
 
 # Media files configuration
